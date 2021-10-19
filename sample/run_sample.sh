de0e25d7aea4b6881a3f6faa2ff102f8f21c2833
mkamei <mkamei@student.42tokyo.jp>
2021-10-19 18:45:15 +0900

    Add sample test_file

--- sample/run_sample.sh
+++ sample/run_sample.sh
@@ -3,8 +3,9 @@
 GNL_CFILES=(get_next_line.c get_next_line_utils.c)
 GNL_INCLUDE_DIR=../
 GNL_DIR=../
-TEST_FILE=../unit_tests/test_files/1_basic.txt
+TEST_FILES=(1_basic.txt 2_noend_nl.txt)
+TEST_FILE_DIR=../unit_tests/test_files
 
 gcc main.c -I${GNL_INCLUDE_DIR} ${GNL_CFILES[@]/#/${GNL_DIR}/}
 echo "---sample---"
-./a.out ${TEST_FILE}
+./a.out ${TEST_FILES[@]/#/${TEST_FILE_DIR}/}
