2b5868deb0e42a27ad0e569dd8aaab2329db80f9
mkamei <mkamei@student.42tokyo.jp>
2021-10-19 18:34:34 +0900

    Edit run_sample.sh

--- sample/run_sample.sh
+++ sample/run_sample.sh
@@ -5,6 +5,6 @@ GNL_INCLUDE_DIR=../
 GNL_DIR=../
 TEST_FILE=../unit_tests/test_files/1_basic.txt
 
-gcc main.c -I${GNL_INCLUDE_DIR} ${GNL_CFILES[@]/#/${GNL_DIR}}
+gcc main.c -I${GNL_INCLUDE_DIR} ${GNL_CFILES[@]/#/${GNL_DIR}/}
 echo "---sample---"
 ./a.out ${TEST_FILE}
